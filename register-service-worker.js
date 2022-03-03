2e4d6c55e567fe5e7c439defcbbcbea90159f8c0
Daniel Dreke <daniel.dreke@mailbox.org>
2022-03-03 06:07:58 +0530

    fix link to manifest.json and .js file

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,7 +1,7 @@
 // source: https://github.com/piotrswigon/pwa-demo/blob/master/script.js
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function(err) {
