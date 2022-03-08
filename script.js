cfd4f401747b5d212b6ebfa473c47339e4a25d23
Daniel Dreke <22680274+danieldreke@users.noreply.github.com>
2022-03-09 04:02:41 +0530

    add feature autopause before breathing in

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const BUTTON_PAUSE_RESUME = "pauseresume";
 const BUTTON_START_BREATHING_IN = "startbreathingin";
 const BUTTON_REPLAY_ROUND_2 = "replayround2";
 const BUTTON_REPLAY_ROUND_3 = "replayround3";
+const CBOX_LABEL_AUTOPAUSE = "cboxlabel";
 
 const STARTTIME_ROUND_2 = 196;
 const STARTTIME_ROUND_3 = 404;
@@ -21,6 +22,7 @@ const ENDTIME_SESSION = 630;
 
 var ytplayer;
 var pausedDueToEndOfSession = false;
+var videoAutoPaused = false;
 
 function onYouTubeIframeAPIReady() {
   createYouTubePlayer();
@@ -54,15 +56,16 @@ function getVideoTime() {
 
 function isCloseToEndOfRetention() {
   var videotime = getVideoTime();
-  var isCloseToEndOfRetention1 = videotime >= 159 && videotime <= 177;
-  var isCloseToEndOfRetention2 = videotime >= 372 && videotime <= 386;
-  var isCloseToEndOfRetention3 = videotime >= 581 && videotime <= 595;
+  var isCloseToEndOfRetention1 = videotime >= 159 && videotime < 177;
+  var isCloseToEndOfRetention2 = videotime >= 372 && videotime < 386;
+  var isCloseToEndOfRetention3 = videotime >= 581 && videotime < 594;
   var isCloseToEndOfRetention_ = isCloseToEndOfRetention1 || isCloseToEndOfRetention2 || isCloseToEndOfRetention3;
   return isCloseToEndOfRetention_;
 }
 
 function onYouTubePlayerReady(event) {
   showElementById(BUTTON_START_SESSION);
+  showElementById(CBOX_LABEL_AUTOPAUSE);
   function checkCurrentTime() {
     if (ytplayer && ytplayer.getCurrentTime) {
       var videotime = getVideoTime();
@@ -74,10 +77,29 @@ function onYouTubePlayerReady(event) {
       }
       if (isCloseToEndOfRetention_) {
         showElementById(BUTTON_PAUSE_RESUME);
+        showElementById(CBOX_LABEL_AUTOPAUSE);
+        showElementById(BUTTON_START_BREATHING_IN);
       }
       else {
         hideElementById(BUTTON_PAUSE_RESUME);
         hideElementById(BUTTON_START_BREATHING_IN);
+        if (videotime > 0) {
+          hideElementById(CBOX_LABEL_AUTOPAUSE);
+        }
+      }
+      var isBreathingInTime = (videotime == STARTTIME_BREATHING_ROUND_1)
+        || (videotime == STARTTIME_BREATHING_ROUND_2)
+        || (videotime == STARTTIME_BREATHING_ROUND_3);
+      if (isBreathingInTime) {
+        autopause = document.getElementById("autopause").checked;
+        var pausevideo = autopause && !videoAutoPaused;
+        if (pausevideo) {
+          ytplayer.pauseVideo();
+          videoAutoPaused = true;
+        }
+      }
+      else {
+        videoAutoPaused = false;
       }
       if (isEndOfRound3) {
         showElementById(BUTTON_REPLAY_ROUND_2);
@@ -150,6 +172,7 @@ function showElementById(elementId) {
 function startSession() {
   playVideoAtTime(0);
   hideElementById(BUTTON_START_SESSION);
+  hideElementById(CBOX_LABEL_AUTOPAUSE);
   showElementById(BUTTON_SKIP_INTRO);
   document.getElementById("ytplayer").classList.remove("disabled-events");
 }
